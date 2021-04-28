4a88cb80cb654271e11f39daa81bde75bd4d13c8
louisenje <louisenje@gmail.com>
2021-04-28 13:54:51 +0300

    created init.py file imported links and login manager details

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,44 @@
-from flask_mail import Message
-from flask import render_template
-from .import mail
+from werkzeug.utils import secure_filename
+from flask import Flask
+from config import config_options
+from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_uploads import UploadSet,configure_uploads,IMAGES
+from flask_mail import Mail
 
+login_manager=LoginManager()
+login_manager.session_protection='strong'
+login_manager.login_view='auth.login'
 
-def mail_message(subject,template,to,**kwargs):
-    sender_email = 'louisenje@gmail.com'
+photos = UploadSet('photos',IMAGES)
+mail=Mail()
+# Initializing Flask Extensions
+bootstrap=Bootstrap()
+db=SQLAlchemy()
 
-    email = Message(subject, sender=sender_email, recipients=[to])
-    email.body= render_template(template + ".txt",**kwargs)
-    email.html = render_template(template + ".html",**kwargs)
-    mail.send(email)
+def create_app(config_name):
+
+    app = Flask(__name__)
+
+    # Creating the app configurations
+    app.config.from_object(config_options[config_name])
+
+    # Initializing flask extensions
+    bootstrap.init_app(app)
+    db.init_app(app)
+    login_manager.init_app(app)
+    mail.init_app(app)
+    # Registering the blueprint
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint,url_prefix = '/authenticate')
+
+    # configure UploadSet
+    configure_uploads(app,photos)
+
+    # Will add the views and forms
+
+    return app
+
